13c50441526e857d5ea462e7f41ec042da5a17dc
Injung Chung <mu29@yeoubi.net>
2019-04-04 22:23:06 +0900

    7-12. 즐겨찾는 글 목록 화면 만들기

--- screens/BookmarkScreen.js
+++ screens/BookmarkScreen.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import {
   SafeAreaView,
-  Text,
   StyleSheet,
 } from 'react-native'
 import Header from '../components/Header'
+import ArticleItem from '../components/ArticleItem'
 
 const BookmarkScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Header title="즐겨찾기" />
+      <ArticleItem article={{ id: 1, title: '김철수', content: '이야기', date: '2019년 4월 5일' }} />
     </SafeAreaView>
   )
 }
